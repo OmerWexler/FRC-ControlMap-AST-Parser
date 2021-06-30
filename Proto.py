12bcca71806ee0812a978761dc450733fff63885
Omer Wexler <omerwexler14@gmail.com>
2021-06-30 19:25:40 +0300

    Still WIP

--- Proto.py
+++ Proto.py
@@ -1,5 +1,6 @@
 import re
 import javalang
+from numpy import append
 
 FILENAME = r'E:\Desktop\workspace\software\java operation parser\ControlMap.java'
 
@@ -10,20 +11,62 @@ def get_file_ast(filename):
     return tree
 
 
-def find_class(class_name, tree):
+def find_class(name, tree):
     for path, node in tree.filter(javalang.tree.ClassDeclaration):
-        if node.name == class_name:
+        if node.name == name:
             return path, node
 
 
-def get_class_methods(class_node):
+def match_expr(match_dict, invocation):
     pass
+
+
+def as_pascal_case(string: str):
+    pascal = ""
+
+    for (i, char) in enumerate(string):
+        if i == 0:
+            char = char.upper()
+        elif char.isupper():
+            pascal += " "
+
+        pascal += char
+
+    return pascal
+
+
+def parse_class_methods(class_node, button_match_dict):
+    out = {}
+
+    for method in class_node.methods:
+        if method.return_type.name != 'boolean':
+            continue
+
+        relevant_buttons = []
+
+        for _, ret in method.filter(javalang.tree.ReturnStatement):
+            for _, invocation in ret.filter(javalang.tree.MethodInvocation):
+                
+                as_str = f"{invocation.member}("
+                for _, ref in invocation.filter(javalang.tree.MemberReference):
+                    as_str += f"{ref.qualifier}.{ref.member}, "
+
+                as_str = as_str[:-2] + ")"
+
+                button = match_expr(button_match_dict, as_str)
+                
+                if button != None:
+                    relevant_buttons.append(button)
+
+        out[as_pascal_case(method.name)] = relevant_buttons
 
 
 def main():
     tree = get_file_ast(FILENAME)
     _, operator = find_class("Operator", tree)
     _, driver = find_class("Driver", tree)
+
+    m = parse_class_methods(driver)
     
 
 if __name__ == "__main__":
